c3adb6f4982d32c14e652ccbb2fddb9e333372ed
ChanhMN <chanhnm93@gmail.com>
2025-10-05 16:36:02 +0700

    Hoan thanh Assignment2-cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,30 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+## makeCacheMatrix: Tạo đối tượng đặc biệt lưu trữ ma trận và nghịch đảo
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+## cacheSolve: Tính hoặc lấy nghịch đảo từ bộ nhớ đệm
+cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if (!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setinverse(inv)
+    inv
+}
